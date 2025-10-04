4b51bda1dbc5cf640033ccca22e3289ddb4b91bd
Alex Springer <jalexspringer@users.noreply.github.com>
2025-10-04 23:17:36 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,81 @@
 #[allow(unused_imports)]
+use std::{env, fs};
 use std::io::{self, Write};
+use std::path::PathBuf;
+use std::os::unix::fs::PermissionsExt;
 
-enum BuiltIn {
-    Exit(String),
-    Echo,
-    Type,
+const BUILTINS: [&str; 3] = ["exit", "echo", "type"];
+
+enum Command {
+    Exit,
+    Echo(String),
+    Type(String),
+    NotFound(String),
 }
 
+impl Command {
+    fn from_input(input: String) -> Self {
+	let input = input.trim();
+	
+	// Exit clause
+	if input.starts_with("exit 0") { Self::Exit  }
+
+	// Echo
+	else if input.starts_with("echo") {
+	    if let Some(to_echo) = input.strip_prefix("echo ") {
+		Self::Echo(to_echo.trim().to_string())
+	    } else {
+		Self::Echo(String::from("What do you want me to echo?"))
+	    }
+
+	// Type
+	} else if input.starts_with("type") {
+	    if let Some(type_of) = input.strip_prefix("type ") {
+		Self::Type(type_of.trim().to_string())
+	    } else {
+		Self::Type(String::from(""))}
+	}
+
+	// Not a builtin
+	else {
+	    Self::NotFound(input.to_string())
+	}
+    }
+}
+
+
+fn find_file_in_dir(file: &str, dir: &str) -> Option<PathBuf> {
+    let Ok(entries) = fs::read_dir(dir) else {
+        return None;
+    };
+    
+    for entry in entries {
+        let full_path = entry.expect("No path?").path();
+        if full_path.file_stem().unwrap() == file {
+            return Some(full_path);
+        }
+    }
+    None
+}
+
+
+fn find_executable(x: &str) -> Option<PathBuf>{
+    let path_string = env::var("PATH").expect("No path is set");
+    let path: Vec<&str> = path_string.split(":").collect();
+    for dir in path {
+	if let Some(path_to_file) = find_file_in_dir(x, dir) {
+	    let mode = fs::metadata(&path_to_file).unwrap().permissions().mode();
+	    if  mode & 0o100 != 0 {
+		return Some(path_to_file)
+	    }
+	}
+    }
+    // No executable in path
+    None
+}
+
+
 fn main() {
-    let builtin = vec!["exit", "echo", "type"];
     loop {
 	print!("$ ");
 	io::stdout().flush().unwrap();
@@ -16,28 +83,24 @@ fn main() {
 	// Wait for user input
 	let mut input = String::new();
 	io::stdin().read_line(&mut input).unwrap();
-	
-	// Exit clause
-	if input.contains("exit 0") { break }
+	let command = Command::from_input(input);
 
-	// Echo
-	else if input.starts_with("echo") {
-	    println!("{}", input.strip_prefix("echo ")
-		     .expect("Nothing to echo!")
-		     .to_string()
-		     .trim());
-	    continue;
-	} else if input.starts_with("type") {
-	    let type_of = input.strip_prefix("type ")
-		.expect("Type of what mate?").trim();
-	    if builtin.contains(&type_of) {
-		println!("{} is a shell builtin", type_of);
-	    } else {
-		println!("{}: not found", type_of);
-	    }
-	    continue;
+	match command {
+	    Command::Exit => break,
+	    Command::Echo(x) => println!("{}", x),
+	    Command::Type(x) => {
+		// Builtin takes priority
+		if BUILTINS.contains(&x.as_str()) { println!("{} is a shell builtin", x) }
+
+		// Match the path
+		else if let Some(executable) = find_executable(&x) {
+  			println!("{x} is {}", executable.display());	
+  		    } else {
+  			println!("{}: not found", x);
+  		    }
+	    },
+	    Command::NotFound(x) => println!("{}: command not found", x),
 	}
-
-	println!("{}: command not found", input.trim());
     }
 }
+
