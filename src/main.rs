5358f0adc764df9a1bec52093f2c2a0dce0c77aa
Alex Springer <jalexspringer@users.noreply.github.com>
2025-10-04 19:56:11 +0100

    echo

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,16 @@ fn main() {
 	let mut input = String::new();
 	io::stdin().read_line(&mut input).unwrap();
 	
+	// Exit clause
 	if input.contains("exit 0") { break }
+	else if input.starts_with("echo") {
+	    println!("{}", input.strip_prefix("echo ")
+		     .expect("Nothing to echo!")
+		     .to_string()
+		     .trim());
+	    
+	    continue;
+	}
 
 	println!("{}: command not found", input.trim());
     }
