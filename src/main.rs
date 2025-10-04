730bd7a79d34845a08b669e6cf67e79a70dfc511
Alex Springer <jalexspringer@users.noreply.github.com>
2025-10-04 20:11:54 +0100

    type

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum BuiltIn {
+    Exit(String),
+    Echo,
+    Type,
+}
+
 fn main() {
+    let builtin = vec!["exit", "echo", "type"];
     loop {
 	print!("$ ");
 	io::stdout().flush().unwrap();
@@ -12,12 +19,22 @@ fn main() {
 	
 	// Exit clause
 	if input.contains("exit 0") { break }
+
+	// Echo
 	else if input.starts_with("echo") {
 	    println!("{}", input.strip_prefix("echo ")
 		     .expect("Nothing to echo!")
 		     .to_string()
 		     .trim());
-	    
+	    continue;
+	} else if input.starts_with("type") {
+	    let type_of = input.strip_prefix("type ")
+		.expect("Type of what mate?").trim();
+	    if builtin.contains(&type_of) {
+		println!("{} is a shell builtin", type_of);
+	    } else {
+		println!("{}: not found", type_of);
+	    }
 	    continue;
 	}
 
